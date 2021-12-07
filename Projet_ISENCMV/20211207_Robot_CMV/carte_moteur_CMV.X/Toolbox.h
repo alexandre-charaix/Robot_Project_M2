7250fdf1c6d69e9cedd55d37b33c20cf15baa694
Alexandre-Charaix <78098851+alexandre-charaix@users.noreply.github.com>
2021-12-07 13:17:55 +0100

    First Commit
    First Commit (timer / leds / left motor)

---
+++ Projet_ISENCMV/20211207_Robot_CMV/carte_moteur_CMV.X/Toolbox.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   Toolbox.h
+ * Author: CMV_ISEN
+ *
+ * Created on 7 décembre 2021, 08:37
+ */
+
+#ifndef TOOLBOX_H
+#define	TOOLBOX_H
+
+#define PI 3.141592653589793
+
+float Abs(float);
+float Max(float, float);
+float Min(float, float);
+float LimitToInterval(float, float, float);
+float RadianToDegree(float);
+float DegreeToRadian(float);
+
+
+
+#endif	/* TOOLBOX_H */
+
