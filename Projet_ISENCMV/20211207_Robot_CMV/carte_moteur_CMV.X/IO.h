bcad28d35a306e6383ce16f9a265a8bc7b2d82d6
Alexandre-Charaix <78098851+alexandre-charaix@users.noreply.github.com>
2021-12-07 10:30:51 +0100

    First commit
    Work on timer & leds

---
+++ Projet_ISENCMV/20211207_Robot_CMV/carte_moteur_CMV.X/IO.h
@@ -0,0 +1,19 @@
+/*
+ * File:   IO.h
+ * Author: CMV_ISEN
+ *
+ * Created on 7 décembre 2021, 08:37
+ */
+
+#ifndef IO_H
+#define IO_H
+
+//Affectation des pins des LEDS
+#define LED_ORANGE _LATC10 
+#define LED_BLEUE _LATG7
+#define LED_BLANCHE _LATG6
+
+// Prototypes fonctions
+void InitIO();
+
+#endif /* IO_H */
